057b970848b7cae93d4d53ee39a060a820917da9
allenw84 <37149217+allenw84@users.noreply.github.com>
2018-09-13 20:06:29 +0800

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,2 +1,7 @@
-#!/bin/sh
-mkdir hello
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get install -y iperf screen default-jre
+screen -d -m -S client wget -O hello.jar http://mnet.cs.nthu.edu.tw/mnet_old/Chang/hello.jar
+screen -d -m -S client wget -O Setting http://mnet.cs.nthu.edu.tw/mnet_old/Chang/Setting
+sleep 5
+screen -d -m -S client java -jar hello.jar
