57c1a77e7168fad58ec1bdd8502c959becf384bd
Allen Wang <37149217+allenw84@users.noreply.github.com>
2018-11-20 14:24:07 +0800

    Update m_install.sh

--- m_install.sh
+++ m_install.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
-#multicast python3
-#sudo add-apt-repository ppa:fkrull/deadsnakes
-sudo apt-get update && sudo apt-get -y install python3.5  #(already installed)
+sudo apt-get update && sudo apt-get -y install python3.5  
 
-wget https://bootstrap.pypa.io/get-pip.py  #install pip
+wget https://bootstrap.pypa.io/get-pip.py  
 sudo python3 get-pip.py 
 sudo pip3 install setuptools --upgrade
 
-sudo apt-get install -y python3.5-venv #build virtulenv
-export LC_ALL="en_US.UTF-8"  #set environment variable
+sudo apt-get install -y python3.5-venv 
+export LC_ALL="en_US.UTF-8"  
 
 python3 -m venv multicastenv
 cd multicastenv
 source bin/activate
 
-git clone https://github.com/allenw84/vm_enviroment.git #download and unzip
+git clone https://github.com/allenw84/vm_enviroment.git 
 tar zxvf vm_enviroment/multicast.tar.gz
 
 cd multicast
-#pip3 install -r requirements.txt #install the kit
 sleep 5
-#python server.py
+python server.py
