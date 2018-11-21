d24c0abf7871ce0ed7036d1fd87fd795b31fe3a3
Allen Wang <37149217+allenw84@users.noreply.github.com>
2018-11-21 16:21:40 +0800

    Update u_install.sh

--- u_install.sh
+++ u_install.sh
@@ -2,7 +2,7 @@
 
 #unicast python3
 #sudo add-apt-repository ppa:fkrull/deadsnakes
-sudo apt-get update && sudo apt-get -y install python3.5  (already installed)
+sudo apt-get update && sudo apt-get -y install python3.5  #(already installed)
 
 wget https://bootstrap.pypa.io/get-pip.py  #install pip
 sudo python3 get-pip.py 
@@ -15,8 +15,8 @@ python3 -m venv unicastenv
 cd unicastenv
 source bin/activate
 
-git clone https://github.com/allenw84/vm_enviroment.git #download and unzip
-tar zxvf vm_enviroment/unicast.tar.gz
+git clone https://github.com/allenw84/python-git.git #download and unzip
+tar zxvf python-git/unicast.tar.gz
 
 cd unicast
 pip3 install -r requirements.txt #install the kit
