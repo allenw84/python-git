1df38b581723f3a0256cee0230f1477b7fb9507b
Allen Wang <37149217+allenw84@users.noreply.github.com>
2018-12-27 21:34:30 +0800

    Add files via upload

---
+++ z_install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+sudo apt-get update && sudo apt-get -y install python3.5  (already installed)
+
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python3 get-pip.py 
+sudo pip3 install setuptools --upgrade
+
+sudo apt-get -y install python3.5-venv
+export LC_ALL="en_US.UTF-8"
+
+python3 -m venv unicastenv
+cd unicastenv
+source bin/activate
+
+git clone https://github.com/allenw84/python-git.git
+tar zxvf python-git/xnicast.tar.gz
+cd xnicast
+pip3 install -r requirements.txt
+sleep 5
+python app.py
